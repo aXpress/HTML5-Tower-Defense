20924e1083a5405a795c3c2affe0534703190bec
Abraham Cheng <31293817+aXpress@users.noreply.github.com>
2021-05-03 07:14:01 -0700

    Update MapCreation.js

--- src/scenes/MapCreation.js
+++ src/scenes/MapCreation.js
@@ -113,81 +113,7 @@ var MapCreation = new Phaser.Class({
         butContainer.add(waterButton);
         butContainer.add(waterText);
 
-        // // Tower selection container
-        // var towerContainer = this.add.container(400,800);
-        
-        // var fireTowerBtn = this.add.rectangle(0, 0, 150, butHeight, 0xff0000);
-        // var fireBtnTxt = this.add.text(0, 0, "Fire",{font: '18pt Arial', fill: '0xff0000'});
-        // fireBtnTxt.setOrigin(0.5,0.5);
-        // fireTowerBtn.setStrokeStyle(5,0xff0000);
-        // fireTowerBtn.setInteractive().on('pointerover', function(event) {this.setFillStyle(0xffffff, .75);});
-        // fireTowerBtn.setInteractive().on('pointerout', function(event) {this.setFillStyle(0xff0000, 1);});
-        // fireTowerBtn.setInteractive().on('pointerdown', function(event) {
-        //     curBut = 'fireTower';
-        //     changeActive(this.parentContainer.list);
-        //     this.setStrokeStyle(5,0xffbb00);
-        // });
-
-        // var waterTowerBtn = this.add.rectangle(200, 0, 150, butHeight, 0x00AAFF);
-        // var waterBtnTxt = this.add.text(200, 0, "Water",{font: '18pt Arial', fill: '0xffffff'});
-        // waterBtnTxt.setOrigin(0.5,0.5);
-        // waterTowerBtn.setStrokeStyle(5,0xff0000);
-        // waterTowerBtn.setInteractive().on('pointerover', function(event) {this.setFillStyle(0xffffff, .75);});
-        // waterTowerBtn.setInteractive().on('pointerout', function(event) {this.setFillStyle(0x00AAFF, 1);});
-        // waterTowerBtn.setInteractive().on('pointerdown', function(event) {
-        //     curBut = 'waterTower';
-        //     changeActive(this.parentContainer.list);
-        //     this.setStrokeStyle(5,0xffbb00);
-        // });
-
-        // var windTowerBtn = this.add.rectangle(400, 0, 150, butHeight, 0x00C40C);
-        // var windBtnTxt = this.add.text(400, 0, "Wind",{font: '18pt Arial', fill: '0xffffff'});
-        // windBtnTxt.setOrigin(0.5,0.5);
-        // windTowerBtn.setStrokeStyle(5,0xff0000);
-        // windTowerBtn.setInteractive().on('pointerover', function(event) {this.setFillStyle(0xffffff, .75);});
-        // windTowerBtn.setInteractive().on('pointerout', function(event) {this.setFillStyle(0x00C40C, 1);});
-        // windTowerBtn.setInteractive().on('pointerdown', function(event) {
-        //     curBut = 'windTower';
-        //     changeActive(this.parentContainer.list);
-        //     this.setStrokeStyle(5,0xffbb00);
-        // });
-
-        // var elecTowerBtn = this.add.rectangle(600, 0, 150, butHeight, 0xFFD800);
-        // var elecBtnTxt = this.add.text(600, 0, "Electric",{font: '18pt Arial', fill: '0xffffff'});
-        // elecBtnTxt.setOrigin(0.5,0.5);
-        // elecTowerBtn.setStrokeStyle(5,0xff0000);
-        // elecTowerBtn.setInteractive().on('pointerover', function(event) {this.setFillStyle(0xffffff, .75);});
-        // elecTowerBtn.setInteractive().on('pointerout', function(event) {this.setFillStyle(0xFFD800, 1);});
-        // elecTowerBtn.setInteractive().on('pointerdown', function(event) {
-        //     curBut = 'elecTower';
-        //     changeActive(this.parentContainer.list);
-        //     this.setStrokeStyle(5,0xffbb00);
-        // });
-
-        // var iceTowerBtn = this.add.rectangle(800, 0, 150, butHeight, 0x8AFFFD);
-        // var iceBtnTxt = this.add.text(800, 0, "Ice",{font: '18pt Arial', fill: '0xffffff'});
-        // iceBtnTxt.setOrigin(0.5,0.5);
-        // iceTowerBtn.setStrokeStyle(5,0xff0000);
-        // iceTowerBtn.setInteractive().on('pointerover', function(event) {this.setFillStyle(0xffffff, .75);});
-        // iceTowerBtn.setInteractive().on('pointerout', function(event) {this.setFillStyle(0x8AFFFD, 1);});
-        // iceTowerBtn.setInteractive().on('pointerdown', function(event) {
-        //     curBut = 'iceTower';
-        //     changeActive(this.parentContainer.list);
-        //     this.setStrokeStyle(5,0xffbb00);
-        // });
-
-        // towerContainer.add(fireTowerBtn);
-        // towerContainer.add(fireBtnTxt);
-        // towerContainer.add(waterTowerBtn);
-        // towerContainer.add(waterBtnTxt);
-        // towerContainer.add(windTowerBtn);
-        // towerContainer.add(windBtnTxt);
-        // towerContainer.add(elecTowerBtn);
-        // towerContainer.add(elecBtnTxt);
-        // towerContainer.add(iceTowerBtn);
-        // towerContainer.add(iceBtnTxt);
-
-
+    
         // helper function for menu buttons. Changes the outline color.
         var changeActive = function (pointer)
         {
@@ -374,9 +300,6 @@ var MapCreation = new Phaser.Class({
                     repeat: -1
                 }); 
             }
-            // else if (curBut == 'fireTower') {
-            //     placeTower({scene: this, x: pointer.x, y: pointer.y, element: curBut});
-            // }
         });
         
         // This creates a screenshot of the map. Right now it is just for testing. At some
@@ -540,21 +463,3 @@ var MapCreation = new Phaser.Class({
         //graphics.moveTo(curve2);
     }
 });
-
-// function placeTower(scene, x, y, selected) {
-//     if(selected = "fireTower"){
-//         var tower = new Tower(scene, x, y, "0xFF0000");
-//     }
-//     else if(selected = "waterTower") {
-//         var tower = new Tower(scene, x, y, "0x00AAFF");
-//     }
-//     else if(selected = "windTower") {
-//         var tower = new Tower(scene, x, y, "0x00C40C");
-//     }
-//     else if(selected = "elecTower") {
-//         var tower = new Tower(scene, x, y, "0xFFD800");
-//     }
-//     else if(selected = "iceTower") {
-//         var tower = new Tower(scene, x, y, "0x8AFFFD");
-//     }
-// }
