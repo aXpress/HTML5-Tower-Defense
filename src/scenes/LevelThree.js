836be9fe9b8ebe17bb9835c3c30cdcbc5c0bccb4
Eric Johnson <evjohnson227@gmail.com>
2021-05-24 12:33:38 -0700

    enemy wave creation

--- src/scenes/LevelThree.js
+++ src/scenes/LevelThree.js
@@ -1,3 +1,5 @@
+var enemies;
+var ENEMY_SPEED = 1/100000;
 var path;
 
 var LevelThree = new Phaser.Class({
@@ -32,14 +34,32 @@ var LevelThree = new Phaser.Class({
         
         // the path for our enemies
         // parameters are the start x and y of our path
-        path = this.add.path(96, -32);
-        path.lineTo(96, 164);
-        path.lineTo(480, 164);
-        path.lineTo(480, 544);
+        path = this.add.path(0, 450);
+        path.lineTo(450, 450);
+        path.lineTo(900, 450);
+        path.lineTo(1600, 450);
         
         graphics.lineStyle(3, 0xffffff, 1);
         // visualize the path
         path.draw(graphics);
+
+        enemies = this.add.group({ classType: Enemy, runChildUpdate: true });
+	    this.nextEnemy = 0;
+    },
+    update: function(time, delta)
+    {
+        if (time > this.nextEnemy)
+        {
+            var enemy = enemies.get();
+            if (enemy)
+            {
+                enemy.setActive(true);
+                enemy.setVisible(true);
+                enemy.startOnPath();
+
+                this.nextEnemy = time + 2000;
+            }       
+        }
     }
 
 });
@@ -54,12 +74,36 @@ var Enemy = new Phaser.Class({
     {
         Phaser.GameObjects.Image.call(this, scene, 0, 0);
         this.setTexture('wraithEnemy');
-        this.hp = 100;
+        this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
+        this.hp = 0;
 
+    },
+    startOnPath: function ()
+    {
+        // set the t parameter at the start of the path
+        this.follower.t = 0;
+        this.hp = 100;
+        
+        // get x and y of the given t point            
+        path.getPoint(this.follower.t, this.follower.vec);
+        
+        // set the x and y of our enemy to the received from the previous step
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+        
     },
     update: function (time, delta)
     {
+        this.follower.t += ENEMY_SPEED * delta;
+        path.getPoint(this.follower.t, this.follower.vec);
         
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+
+        if (this.follower.t >= 1)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        }
     }
 
 });
+
