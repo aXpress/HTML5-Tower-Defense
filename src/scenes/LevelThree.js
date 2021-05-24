fe24df444aa13eae565c7465af0d533fd8458fd9
Eric Johnson <evjohnson227@gmail.com>
2021-05-24 12:17:53 -0700

    add empty enemy object

--- src/scenes/LevelThree.js
+++ src/scenes/LevelThree.js
@@ -1,3 +1,5 @@
+var path;
+
 var LevelThree = new Phaser.Class({
 
     Extends: Phaser.Scene,
@@ -11,6 +13,8 @@ var LevelThree = new Phaser.Class({
 
     preload: function ()
     {
+        this.load.image('imgMainMenuButton', 'src/assets/imgMainMenuButton.png');
+        this.load.image('wraithEnemy', 'src/assets/enemies/wraith.png');
     },
 
     create: function ()
@@ -21,6 +25,41 @@ var LevelThree = new Phaser.Class({
         mainMenuButton.setInteractive().on('pointerover', function(event) {this.setTint(0xC0C0C0);});
         mainMenuButton.setInteractive().on('pointerout', function(event) {this.clearTint();});
         mainMenuButton.setInteractive().on('pointerdown', function() {this.scene.start('MainMenu')}, this);
+
+        // this graphics element is only for visualization, 
+        // its not related to our path
+        var graphics = this.add.graphics();    
+        
+        // the path for our enemies
+        // parameters are the start x and y of our path
+        path = this.add.path(96, -32);
+        path.lineTo(96, 164);
+        path.lineTo(480, 164);
+        path.lineTo(480, 544);
+        
+        graphics.lineStyle(3, 0xffffff, 1);
+        // visualize the path
+        path.draw(graphics);
+    }
+
+});
+
+var Enemy = new Phaser.Class({
+ 
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+
+    function Enemy (scene)
+    {
+        Phaser.GameObjects.Image.call(this, scene, 0, 0);
+        this.setTexture('wraithEnemy');
+        this.hp = 100;
+
+    },
+    update: function (time, delta)
+    {
+        
     }
 
 });
